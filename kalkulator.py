24a5c487293bd1b595cb39c814ad5dfd19ce10b2
Jakub Twardy <kubus2@gazeta.pl>
2015-05-20 10:53:17 +0200

    Miejsce

--- kalkulator.py
+++ kalkulator.py
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
 def dodawanie(a,b):
-=======
-def dodawanie():
->>>>>>> zmiana_funkcji_dodawania
 	return a+b
+def get_info():
+	print ("To jest prosty...")
+get_info()
 print('Podaj pierwszą liczbę:')
 a=int(input())
 print('Podaj drugą liczbę:')
 b=int(input())
-<<<<<<< HEAD
-
 print(dodawanie(a,b))
-=======
->>>>>>> zmiana_funkcji_dodawania
 
